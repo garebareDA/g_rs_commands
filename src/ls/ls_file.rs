7e28a8ec5af3a09025d751e1e6591e8d1394162d
garebareDA <garedaiki@gmail.com>
2022-03-12 01:55:02 +0900

    impl File

--- src/ls/ls_file.rs
+++ src/ls/ls_file.rs
@@ -2,26 +2,38 @@ use super::ls_commands::LsCommands;
 use std::fs;
 use std::path::Path;
 
-struct File {
-    name: String,
-    is_dir: bool,
+#[derive(Debug)]
+pub(crate) struct File {
+  name: String,
+  is_dir: bool,
 }
 
 impl File {
-    pub fn new (name: String, is_dir: bool) -> File {
-        File {
-            name,
-            is_dir,
-        }
-    }
+  pub fn new(name: String, is_dir: bool) -> File {
+    File { name, is_dir }
+  }
+
+  pub fn get_name(&self) -> &str {
+    &self.name
+  }
+
+  pub fn is_dir(&self) -> bool {
+    self.is_dir
+  }
 }
 
 impl LsCommands {
-  pub(crate) fn read_dir<P: AsRef<Path>>(&self, path:P) -> Vec<String> {
-    let files = fs::read_dir(path).unwrap().map(|entry|{
-      let entry = entry.unwrap();
-      return entry.file_name().into_string().unwrap();
-    }).collect();
+  pub(crate) fn read_dir<P: AsRef<Path>>(&self, path: P) -> Vec<File> {
+    let files = fs::read_dir(path)
+      .unwrap()
+      .map(|entry| {
+        let entry = entry.unwrap();
+        return File::new(
+          entry.file_name().into_string().unwrap(),
+          entry.path().is_dir(),
+        );
+      })
+      .collect();
 
     println!("{:?}", files);
     return files;
