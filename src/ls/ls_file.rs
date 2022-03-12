46cbbd6e07a5ad8cc0e9e66d76f060b1a714d191
garebareDA <garedaiki@gmail.com>
2022-03-12 21:00:06 +0900

    fix:ls_command

--- src/ls/ls_file.rs
+++ src/ls/ls_file.rs
@@ -1,16 +1,42 @@
 use super::ls_commands::LsCommands;
-use std::fs;
+use std::os::unix::prelude::MetadataExt;
 use std::path::Path;
+use std::{fs, os::unix::prelude::PermissionsExt};
+use users::{get_group_by_gid, get_user_by_uid};
 
 #[derive(Debug)]
 pub(crate) struct File {
   name: String,
+  group: String,
+  user: String,
+  mode: u32,
+  permission: u32,
+  modification_time: i64,
+  is_hidden: bool,
   is_dir: bool,
 }
 
 impl File {
-  pub fn new(name: String, is_dir: bool) -> File {
-    File { name, is_dir }
+  pub fn new(
+    name: &str,
+    group: &str,
+    user: &str,
+    mode: u32,
+    permission: u32,
+    modification_time: i64,
+    is_hidden: bool,
+    is_dir: bool,
+  ) -> File {
+    File {
+      name: name.to_string(),
+      group: group.to_string(),
+      user: user.to_string(),
+      mode: mode,
+      permission: permission,
+      modification_time: modification_time,
+      is_hidden: is_hidden,
+      is_dir: is_dir,
+    }
   }
 
   pub fn get_name(&self) -> &str {
@@ -28,9 +54,24 @@ impl LsCommands {
       .unwrap()
       .map(|entry| {
         let entry = entry.unwrap();
+        let user = get_user_by_uid(entry.metadata().unwrap().uid()).unwrap();
+        let group = get_group_by_gid(entry.metadata().unwrap().gid()).unwrap();
+        let mode = entry.metadata().unwrap().mode();
+        let permission = entry.metadata().unwrap().permissions().mode();
+        let name = entry.file_name().into_string().unwrap();
+        let is_dir = entry.file_type().unwrap().is_dir();
+        let modification_time = entry.metadata().unwrap().mtime();
+        let is_hidden = name.starts_with(".");
+
         return File::new(
-          entry.file_name().into_string().unwrap(),
-          entry.path().is_dir(),
+          &name,
+          &group.name().to_os_string().into_string().unwrap(),
+          &user.name().to_os_string().into_string().unwrap(),
+          mode,
+          permission,
+          modification_time,
+          is_hidden,
+          is_dir,
         );
       })
       .collect();
