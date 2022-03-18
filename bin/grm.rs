4e1c451c94faaca36a446dec7b27eb1b8b69bdc1
garebareDA <garedaiki@gmail.com>
2022-03-19 01:32:52 +0900

    add:grm test

--- bin/grm.rs
+++ bin/grm.rs
@@ -1,5 +1,12 @@
 extern crate g_rs_commands;
+use g_rs_commands::rm::rm_commands;
+use g_rs_commands::parser::command_line::CommandLine;
+use g_rs_commands::parser::commands::Commands;
 
 fn main() {
-  
+  let rm_commands = rm_commands::RmCommands::new(CommandLine::new());
+  match rm_commands.run() {
+    Ok(_) => {},
+    Err(e) => println!("{}", e),
+  }
 }
--- src/rm/mod.rs
+++ src/rm/mod.rs
@@ -1,2 +1,40 @@
 pub mod rm_commands;
 pub mod rm_file;
+
+
+#[cfg(test)]
+pub mod tests {
+  use crate::parser::commands::Commands;
+  use crate::parser::command_line::CommandLine;
+  use crate::rm::rm_commands::RmCommands;
+  use std::fs;
+  use std::fs::File;
+
+  #[test]
+  fn rm_file() {
+    File::create("test.txt").unwrap();
+    let mut command_line = CommandLine::new();
+    command_line.set_name("rm");
+    command_line.set_args(vec!["test.txt".into()]);
+    command_line.set_options(vec![]);
+    let rm_commands = RmCommands::new(command_line);
+    match rm_commands.run() {
+      Ok(_) => {}
+      Err(e) => panic!("{}", e),
+    }
+  }
+
+  #[test]
+  fn rm_dir() {
+    fs::create_dir("test_dir").unwrap();
+    let mut command_line = CommandLine::new();
+    command_line.set_name("rm");
+    command_line.set_args(vec!["test_dir".into()]);
+    command_line.set_options(vec![]);
+    let rm_commands = RmCommands::new(command_line);
+    match rm_commands.run() {
+      Ok(_) => {}
+      Err(e) => panic!("{}", e),
+    }
+  }
+}
