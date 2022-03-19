7edba8bccb98cde2c605f495232019f27413b6a1
garebareDA <garedaiki@gmail.com>
2022-03-19 22:37:25 +0900

    add:option -r -i

--- src/rm/rm_commands.rs
+++ src/rm/rm_commands.rs
@@ -3,13 +3,34 @@ use crate::parser::commands;
 
 pub struct RmCommands {
   command_line: CommandLine,
+  is_reverso: bool,
+  is_interactive: bool,
 }
 
 impl RmCommands {
   pub fn new(command_line: CommandLine) -> RmCommands {
-    RmCommands {
+    let mut command = RmCommands {
       command_line: command_line,
+      is_reverso: false,
+      is_interactive: false,
+    };
+
+    let options = command.command_line.get_options();
+    for option in options.iter() {
+      if option == "-r" {
+        command.is_reverso = true;
+      }
+
+      if option == "-i" {
+        command.is_interactive = true;
+      }
     }
+
+    return command;
+  }
+
+  pub fn get_interactive(&self) -> bool {
+    return self.is_interactive;
   }
 }
 
@@ -45,7 +66,11 @@ impl commands::Commands for RmCommands {
       return Ok(());
     };
 
-    self.remove_file(&args[0])?;
+    if self.is_reverso {
+      self.remove_reverso(&args[0])?;
+    } else {
+      self.remove_file(&args[0])?;
+    }
 
     Ok(())
   }
--- src/rm/rm_file.rs
+++ src/rm/rm_file.rs
@@ -2,9 +2,13 @@ use super::rm_commands::RmCommands;
 use std::path::Path;
 
 impl RmCommands {
-  pub (crate) fn remove_file<P: AsRef<Path>>(&self, path:P) -> Result<(), String> {
+  pub(crate) fn remove_file<P: AsRef<Path>>(&self, path: P) -> Result<(), String> {
     let path = path.as_ref();
     if path.exists() {
+      if !self.remove_interactive() {
+        return Ok(());
+      }
+
       if path.is_file() {
         return std::fs::remove_file(path).ok().ok_or(format!(
           "grm: cannot remove '{}' : Permission denied",
@@ -19,5 +23,49 @@ impl RmCommands {
     }
 
     return Err("grm: cannot remove '{}' : No such a file or directory".to_string());
+  }
+
+  pub fn remove_reverso<P: AsRef<Path>>(&self, path: P) -> Result<(), String> {
+    let path = path.as_ref();
+    if path.exists() {
+      if path.is_dir() {
+        self.remove_visit_dir(path)?;
+      } else {
+        return Err("grm -r: cannot remove '{}' : No directory".to_string());
+      }
+    }
+    return Err("grm: cannot remove '{}' : No such a file or directory".to_string());
+  }
+
+  fn remove_visit_dir<P: AsRef<Path>>(&self, path: P) -> Result<(), String> {
+    for entry in std::fs::read_dir(path).unwrap() {
+      let entry = entry
+        .ok()
+        .ok_or("grm: cannot remove '{}' : No such a file or directory".to_string())?;
+      let path = entry.path();
+      if path.is_dir() {
+        self.remove_visit_dir(&path)?;
+        self.remove_file(&path)?;
+      } else {
+        self.remove_file(&path)?;
+      }
+    }
+    return Ok(());
+  }
+
+  fn remove_interactive(&self) -> bool {
+    if self.get_interactive() {
+      loop {
+        println!("{}", "Are you sure you want to delete this file? (y/n)");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        if input.trim() == "n" {
+          return false;
+        } else if input.trim() == "y" {
+          break;
+        }
+      }
+    }
+    return true;
   }
 }
