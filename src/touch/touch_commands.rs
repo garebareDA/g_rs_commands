5750bce45d8499d88c17bcea7b514ff76b5f17c5
garebareDA <garedaiki@gmail.com>
2022-03-23 21:30:33 +0900

    add: -a -m options

--- src/touch/touch_commands.rs
+++ src/touch/touch_commands.rs
@@ -3,14 +3,35 @@ use crate::parser::commands::Commands;
 
 pub struct TouchCommands {
     command_line: CommandLine,
+    acsesss_time_update: bool,
+    modification_time_udpate: bool,
 }
 
 impl TouchCommands {
     pub fn new(command_line: CommandLine) -> TouchCommands {
         let mut command = TouchCommands {
             command_line: command_line,
+            acsesss_time_update : false,
+            modification_time_udpate: false,
         };
+
+        for flag in command.command_line.get_options().iter() {
+            match flag.as_str() {
+                "-a" => command.acsesss_time_update = true,
+                "-m" => command.modification_time_udpate = true,
+                _ => (),
+            }
+        }
+
         return command;
+    }
+
+    pub(crate) fn get_acsesss_time_update(&self) -> bool {
+        return self.acsesss_time_update;
+    }
+
+    pub(crate) fn get_modification_time_udpate(&self) -> bool {
+        return self.modification_time_udpate;
     }
 }
 
--- src/touch/touch_file.rs
+++ src/touch/touch_file.rs
@@ -1,15 +1,33 @@
-use std::{path::Path, fs};
+use std::{fs, path::Path};
 
 use super::touch_commands;
-use filetime::{FileTime, set_file_times};
+use filetime::{set_file_times, FileTime};
 impl touch_commands::TouchCommands {
-    pub(crate) fn timestamp_update<P:AsRef<Path> + Copy>(&self, path:P) -> Result<(), String> {
+    pub(crate) fn timestamp_update<P: AsRef<Path> + Copy>(&self, path: P) -> Result<(), String> {
         match fs::metadata(&path) {
             Ok(metadata) => {
-              set_file_times(path, FileTime::now(), FileTime::now()).ok().ok_or(format!("gtouch: {}: cannot set timestamp", &path.as_ref().display()))?;
+                let mtime = if self.get_modification_time_udpate() {
+                    FileTime::from_last_modification_time(&metadata)
+                } else {
+                    FileTime::now()
+                };
+
+                let atime = if self.get_acsesss_time_update() {
+                    FileTime::from_last_access_time(&metadata)
+                } else {
+                    FileTime::now()
+                };
+                set_file_times(path, atime, mtime)
+                    .ok()
+                    .ok_or(format!(
+                        "gtouch: {}: cannot set timestamp",
+                        &path.as_ref().display()
+                    ))?;
             }
             Err(_) => {
-                fs::File::create(&path).ok().ok_or("gtouch: cannot create file")?;
+                fs::File::create(&path)
+                    .ok()
+                    .ok_or("gtouch: cannot create file")?;
             }
         }
         return Ok(());
