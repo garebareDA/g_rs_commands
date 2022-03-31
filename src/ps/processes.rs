4ff730a0a73586e0e4278d3935e9c6d0337bc665
garebareDA <garedaiki@gmail.com>
2022-03-31 23:02:44 +0900

    add:ps command option

--- src/ps/processes.rs
+++ src/ps/processes.rs
@@ -1,12 +1,20 @@
 use super::ps_commands;
 use procfs::process;
+use users::get_user_by_uid;
 
 impl ps_commands::PsCommands {
     pub(crate) fn processes_display(&self) -> Result<(), String> {
         let me = process::Process::myself().unwrap();
         let tps = procfs::ticks_per_second().unwrap();
 
-        println!("{: >5} {: <8} {: >8} {}", "PID", "TTY", "TIME", "CMD");
+        if self.get_is_show_details() {
+            println!(
+                "{: <20} {: >10} {: >10} {: <8} {: <8} {: >8} {: >8} {}",
+                "USER", "PID", "VSZ", "RSS", "TTY", "START", "TIME", "CMD"
+            );
+        } else {
+            println!("{: >5} {: <8} {: >8} {}", "PID", "TTY", "TIME", "CMD");
+        }
 
         let tty = format!("pts/{}", me.stat.tty_nr().1);
         for prc in process::all_processes()
@@ -15,23 +23,114 @@ impl ps_commands::PsCommands {
             .iter()
         {
             let total_time = (prc.stat.utime + prc.stat.stime) as f32 / (tps as f32);
-            if self.get_is_show_all() {
-                if prc.stat.tty_nr().0 != 0 {
-                    println!(
-                        "{: >5} pts/{: <8} {: >8} {}",
-                        prc.stat.pid, prc.stat.tty_nr().1, total_time, prc.stat.comm
-                    );
-                }
+            let user = match get_user_by_uid(prc.owner) {
+                Some(user) => user.name().to_str().unwrap_or("unknown").to_string(),
+                None => "unknown".to_string(),
+            };
+            let pid = prc.stat.pid;
+            let vsize = prc.stat.vsize;
+            let rss = prc.stat.rss;
+            let command_lien = prc
+                .cmdline()
+                .unwrap_or(vec!["unknown".to_string()])
+                .join("");
+            let start_time = prc
+                .stat
+                .starttime()
+                .ok()
+                .ok_or("Failed to get start time")?;
+            let start_time = start_time.format("%H:%M").to_string();
+
+            if self.get_is_terminal() {
+                self.terminal_option_print(prc, total_time);
                 continue;
             }
 
+            if self.get_is_show_all() {
+                self.all_option_print(prc, total_time);
+                continue;
+            }
+
+            if self.get_is_not_terminal() {
+                self.not_terminal_option_print(prc, total_time);
+                continue;
+            }
+
+            if self.get_is_show_details() {
+                if self.get_is_not_terminal() && prc.stat.tty_nr().0 == 0 {
+                    println!(
+                        "{: <20} {: >10} {: >10} {: <8} {: <8} {: >8} {: >8} {}",
+                        user, pid, vsize, rss, tty, start_time, total_time, command_lien
+                    );
+                }
+
+                if self.get_is_terminal() && prc.stat.tty_nr().0 != 0 {
+                    println!(
+                        "{: <20} {: >10} {: >10} {: <8} {: <8} {: >8} {: >8} {}",
+                        user, pid, vsize, rss, tty, start_time, total_time, command_lien
+                    );
+                }
+
+                if self.get_is_show_all() {
+                    println!(
+                        "{: <20} {: >10} {: >10} {: <8} {: <8} {: >8} {: >8} {}",
+                        user, pid, vsize, rss, tty, start_time, total_time, command_lien
+                    );
+                }
+            }
+
             if prc.stat.tty_nr == me.stat.tty_nr {
-                println!(
-                    "{: >5} {: <8} {: >8} {}",
-                    prc.stat.pid, tty, total_time, prc.stat.comm
-                );
+                if self.get_is_show_details() {
+                    println!(
+                        "{: <20} {: >10} {: >10} {: <8} {: <8} {: >8} {: >8} {}",
+                        user, pid, vsize, rss, tty, start_time, total_time, command_lien
+                    );
+                } else {
+                    self.no_option_print(&tty, prc, total_time);
+                }
             }
         }
         return Ok(());
+    }
+
+    fn no_option_print(&self, tty: &str, prc: &process::Process, total_time: f32) {
+        println!(
+            "{: >5} {: <8} {: >8} {}",
+            prc.stat.pid, tty, total_time, prc.stat.comm
+        );
+    }
+
+    fn terminal_option_print(&self, prc: &process::Process, total_time: f32) {
+        if prc.stat.tty_nr().0 == 0 {
+            println!(
+                "{: >5} {: <8} {: >8} {}",
+                prc.stat.pid,
+                prc.stat.tty_nr().1,
+                total_time,
+                prc.stat.comm
+            );
+        }
+    }
+
+    fn not_terminal_option_print(&self, prc: &process::Process, total_time: f32) {
+        if prc.stat.tty_nr().0 != 0 {
+            println!(
+                "{: >5} {: <8} {: >8} {}",
+                prc.stat.pid, "?", total_time, prc.stat.comm
+            );
+        }
+    }
+
+    fn all_option_print(&self, prc: &process::Process, total_time: f32) {
+        let tty = if prc.stat.tty_nr().0 != 0 {
+            format!("pts/{}", prc.stat.tty_nr().1)
+        } else {
+            "?".to_string()
+        };
+
+        println!(
+            "{: >5} {: <8} {: >8} {}",
+            prc.stat.pid, tty, total_time, prc.stat.comm
+        );
     }
 }
--- src/ps/ps_commands.rs
+++ src/ps/ps_commands.rs
@@ -4,6 +4,9 @@ use crate::parser::commands;
 pub struct PsCommands {
     command_line: CommandLine,
     is_show_all: bool,
+    is_terminal: bool,
+    is_not_terminal: bool,
+    is_show_details: bool,
 }
 
 impl PsCommands {
@@ -11,17 +14,16 @@ impl PsCommands {
         let mut commands = PsCommands {
             command_line,
             is_show_all: false,
+            is_terminal: false,
+            is_not_terminal: false,
+            is_show_details: false,
         };
         for flag in commands.command_line.get_options().iter() {
             match flag.as_str() {
-                "-a" => commands.is_show_all = true,
-                _ => (),
-            }
-        }
-
-        for flag in commands.command_line.get_options().iter() {
-            match flag.as_str() {
-                "-a" => commands.is_show_all = true,
+                "-e" => commands.is_show_all = true,
+                "-a" => commands.is_terminal = true,
+                "-x" => commands.is_not_terminal = true,
+                "-l" => commands.is_show_details = true,
                 _ => (),
             }
         }
@@ -32,6 +34,18 @@ impl PsCommands {
     pub fn get_is_show_all(&self) -> bool {
         return self.is_show_all;
     }
+
+    pub fn get_is_terminal(&self) -> bool {
+        return self.is_terminal;
+    }
+
+    pub fn get_is_not_terminal(&self) -> bool {
+        return self.is_not_terminal;
+    }
+
+    pub fn get_is_show_details(&self) -> bool {
+        return self.is_show_details;
+    }
 }
 
 impl commands::Commands for PsCommands {
@@ -39,8 +53,6 @@ impl commands::Commands for PsCommands {
         println!("gps: Print a list of processes to stdout.");
         println!("usage: ps [OPTION]...");
         println!("");
-        println!("  -o, --sort=STRING   sort by STRING");
-        println!("  -r, --reverse       reverse order while sorting");
         println!("  -a, --all           show all processes, even those that are stopped");
         println!("  -v, --version       print version information and exit");
         println!("  -h, --help          display this help and exit");
